b06d2e4ea5dace607a057bb16c1ff6648c2a4857
Sculas <contact@sculas.xyz>
2022-08-19 14:54:48 +0200

    refactor: apply SeaQL/sea-orm#750

--- src/driver/sqlx_common.rs
+++ src/driver/sqlx_common.rs
@@ -1,13 +1,14 @@
 use crate::DbErr;
+use std::sync::Arc;
 
 /// Converts an [sqlx::error] execution error to a [DbErr]
 pub fn sqlx_error_to_exec_err(err: sqlx::Error) -> DbErr {
-    DbErr::Exec(err.to_string())
+    DbErr::Exec(err.to_string(), Some(Arc::new(err)))
 }
 
 /// Converts an [sqlx::error] query error to a [DbErr]
 pub fn sqlx_error_to_query_err(err: sqlx::Error) -> DbErr {
-    DbErr::Query(err.to_string())
+    DbErr::Query(err.to_string(), Some(Arc::new(err)))
 }
 
 /// Converts an [sqlx::error] connection error to a [DbErr]
--- src/error.rs
+++ src/error.rs
@@ -1,12 +1,19 @@
+#[cfg(feature = "sqlx-dep")]
+use sqlx::error::Error as SqlxError;
+use std::sync::Arc;
+
+#[cfg(not(feature = "sqlx-dep"))]
+type SqlxError = ();
+
 /// An error from unsuccessful database operations
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, Clone)]
 pub enum DbErr {
     /// There was a problem with the database connection
     Conn(String),
     /// An operation did not execute successfully
-    Exec(String),
+    Exec(String, Option<Arc<SqlxError>>),
     /// An error occurred while performing a query
-    Query(String),
+    Query(String, Option<Arc<SqlxError>>),
     /// The record was not found in the database
     RecordNotFound(String),
     /// A custom error
@@ -19,14 +26,33 @@ pub enum DbErr {
     Migration(String),
 }
 
+impl PartialEq for DbErr {
+    fn eq(&self, other: &Self) -> bool {
+        self.to_string() == other.to_string()
+    }
+}
+
 impl std::error::Error for DbErr {}
+
+#[cfg(feature = "sqlx-dep")]
+impl DbErr {
+    /// provides the underlying error from sqlx, if available
+    pub fn sqlx_error(&self) -> Option<&SqlxError> {
+        match self {
+            DbErr::Exec(_, source) => source.as_ref(),
+            DbErr::Query(_, source) => source.as_ref(),
+            _ => None,
+        }
+        .map(|s| s.as_ref())
+    }
+}
 
 impl std::fmt::Display for DbErr {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            Self::Conn(s) => write!(f, "Connection Error: {}", s),
-            Self::Exec(s) => write!(f, "Execution Error: {}", s),
-            Self::Query(s) => write!(f, "Query Error: {}", s),
+            Self::Conn(s, ..) => write!(f, "Connection Error: {}", s),
+            Self::Exec(s, ..) => write!(f, "Execution Error: {}", s),
+            Self::Query(s, ..) => write!(f, "Query Error: {}", s),
             Self::RecordNotFound(s) => write!(f, "RecordNotFound Error: {}", s),
             Self::Custom(s) => write!(f, "Custom Error: {}", s),
             Self::Type(s) => write!(f, "Type Error: {}", s),
